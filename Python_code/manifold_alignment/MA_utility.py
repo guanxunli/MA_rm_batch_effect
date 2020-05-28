cec5fdd3acda199b8d8ccb8f8aad0a6b0d1644d0
Guanxun Li <victorguanxun@gmail.com>
2020-05-27 21:56:25 -0500

    Add basic manifold alignment.

--- Python_code/manifold_alignment/MA_utility.py
+++ Python_code/manifold_alignment/MA_utility.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+import scanpy as sc
 
 def manifold_alignment(Wx, Wy, Wxy = None, mu = 0.9, d = 30, normlized = True, eps = 1e-5):
     Wx = np.array(Wx)
@@ -31,3 +33,31 @@ def manifold_alignment(Wx, Wy, Wxy = None, mu = 0.9, d = 30, normlized = True, e
         alignedNet[:,i] /= np.linalg.norm(alignedNet[:,i])
 
     return alignedNet
+
+def data_process(dta, min_genes = 100, min_cells = 10, mt_pct = 10, npcs = 50, oversd = None):
+    adata = sc.AnnData(dta)
+    adata.var_names_make_unique()
+    # quality control
+    sc.pp.filter_cells(adata, min_genes = min_genes)
+    sc.pp.filter_genes(adata, min_cells = min_cells)
+    adata.var['mt'] = adata.var_names.str.startswith('MT-')
+    sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, inplace=True)
+
+    if oversd is not None:
+        mu = np.mean(adata.obs.n_genes_by_counts)
+        sd = np.std(adata.obs.n_genes_by_counts)
+        thres = mu + oversd * sd
+        adata = adata[adata.obs.n_genes_by_counts < thres, :]
+        
+    adata = adata[adata.obs.pct_counts_mt < mt_pct, :]
+    # normalization
+    sc.pp.normalize_total(adata, target_sum=1e4)
+    sc.pp.log1p(adata)
+    # find highly variable gene
+    sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
+    adata.raw = adata
+    adata = adata[:, adata.var.highly_variable]
+    # pca
+    sc.tl.pca(adata, svd_solver='arpack', n_comps = npcs)
+    
+    return adata
