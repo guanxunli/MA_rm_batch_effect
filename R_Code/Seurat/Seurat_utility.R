bdda6bdde3d8981706ce785c174051fab6928e9a
Guanxun Li <victorguanxun@gmail.com>
2020-06-12 11:09:08 -0500

    Add seurat one to one plot.

--- R_Code/Seurat/Seurat_utility.R
+++ R_Code/Seurat/Seurat_utility.R
@@ -46,5 +46,16 @@ seurat_integrate <- function(batch_list, npcs = 50){
   return(batches)
 }
 
+# plot for atacrna
+plot_onetoone <- function(dta_embed, batch){
+  dta_use <- data.frame("x" = dta_embed[, 1], "y" = dta_embed[, 2], "batch" = batch)
+  n <- nrow(dta_embed)/2
+  dta_use$group <- c(1:n, 1:n)
+  p <- ggplot(data = dta_use, aes(x, y, color = batch, group = group)) +
+    geom_point(size = 0.5) +
+    geom_line(color = "black", size = 0.25) +
+    labs(x = "embed1", y = "embed2", color = "data type")
+  return(p)
+}
 
 
--- R_Code/Seurat/atacrna_seurat.R
+++ R_Code/Seurat/atacrna_seurat.R
@@ -48,6 +48,7 @@ batches <- seurat_integrate(batch_list = batch_list, npcs = npcs)
 ## plot results
 dta_use <- as.matrix(batches@assays$integrated@data)
 dta_use <- t(dta_use)
+# dta_use <- readRDS("R_Code/results/seurat_results/atacrna/seurat_atacrna.rds")
 tsne_embeddings <- Rtsne::Rtsne(dta_use, is_distance=FALSE, perplexity=30, num_threads=1, verbose=FALSE)$Y
 umap_embeddings <- umap::umap(dta_use)$layout
 
@@ -61,12 +62,64 @@ p2 <- ggplot(data = NULL, aes(umap_embeddings[, 1], umap_embeddings[, 2], color 
 print(p2)
 
 # save results
-png("R_Code/results/seurat_results/atacrna/tsne_seurat.png",width = 2*1000, height = 800, res = 2*72)
+png("R_Code/Seurat/seurat_results/atacrna/tsne_seurat.png",width = 2*1000, height = 800, res = 2*72)
 print(p1)
 dev.off()
 
-png("R_Code/results/seurat_results/atacrna/umap_seurat.png",width = 2*1000, height = 800, res = 2*72)
+png("R_Code/Seurat/seurat_results/atacrna/umap_seurat.png",width = 2*1000, height = 800, res = 2*72)
 print(p2)
 dev.off()
 
-saveRDS(dta_use, "R_Code/results/seurat_results/atacrna/seurat_atacrna.rds")
+saveRDS(dta_use, "R_Code/Seurat/seurat_results/atacrna/seurat_atacrna.rds")
+
+## plot one to one
+p1 <- ggplot(data = NULL, aes(tsne_embeddings[, 1], tsne_embeddings[, 2], color = metadata$batchlb)) +
+  geom_point(size = 0.5) +
+  labs(x = "T-SNE1", y = "T-SNE2", color = "data type") 
+dta_embd <- tsne_embeddings
+p2 <- plot_onetoone(dta_embd, batch = metadata$batchlb)
+print(plot_grid(p1,p2), nrow = 1)
+png("R_Code/Seurat/seurat_results//atacrna/tsne_harmony_seperate_one2one.png",width = 2*1000, height = 800, res = 2*72)
+print(plot_grid(p1,p2), nrow = 1)
+dev.off()
+
+p1 <- ggplot(data = NULL, aes(umap_embeddings[, 1], umap_embeddings[, 2], color = metadata$batchlb)) +
+  geom_point(size = 0.5) +
+  labs(x = "UMAP1", y = "UMAP2", color = "data type")
+dta_embd <- umap_embeddings
+p2 <- plot_onetoone(dta_embd, batch = metadata$batchlb)
+print(plot_grid(p1,p2), nrow = 1)
+png("R_Code/Seurat/seurat_results//atacrna/umap_seurat_one2one.png",width = 2*1000, height = 800, res = 2*72)
+print(plot_grid(p1,p2), nrow = 1)
+dev.off()
+
+## generate table
+dta <- readRDS("R_Code/Seurat/seurat_results/atacrna/seurat_atacrna.rds")
+n <- nrow(dta)/2
+dta_atac <- dta[1:n, ]
+dta_rna <- dta[-(1:n), ]
+rowname_atac <- rownames(dta_atac)
+rowname_atac_list <- lapply(rowname_atac, strsplit, "_")
+for (i in 1:length(rowname_atac)){
+  rowname_atac[i] <- rowname_atac_list[[i]][[1]][1]
+}
+rownames(dta_atac) <- rowname_atac
+kmeans_rna <- kmeans(dta_rna, centers = 4, nstart = 10)
+rna_cluster <- kmeans_rna$cluster
+distance_mat <- matrix(NA, nrow = n, ncol = n)
+for (i in 1:n){
+  tmp_mat <- matrix(dta_atac[i, ], nrow = n, ncol = ncol(dta_rna), byrow = TRUE)
+  distance_mat[i, ] <- rowSums((tmp_mat - dta_rna)^2)
+}
+atac_cluster <- rep(NA, length(rna_cluster))
+for (i in 1:length(atac_cluster)){
+  index <- order(distance_mat[i, ], decreasing = FALSE)[1:20]
+  cluster_tmp <- rna_cluster[index]
+  tmp <- table(cluster_tmp)
+  tmp_mat <- as.matrix(tmp)
+  atac_cluster[i] <- as.numeric(names(tmp)[which.max(tmp_mat)])
+}
+names(atac_cluster) <- names(rna_cluster)
+table(rna_cluster)
+table(atac_cluster)
+table(rna_cluster, atac_cluster)
